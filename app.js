ecbbfe125eb9bb7f4f6a6c3e8b417f114cae3236
Alex Tong <tongsalex@gmail.com>
2020-06-13 17:18:03 -0400

    setting up auth

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
+// mongo connections and server technologies
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const passport = require('passport');
 
+// parses json
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
+// our DB password keys to connect
 const db = require('./config/keys').mongoURI;
+// ROUTE IMPORTS
+const users = require("./routes/api/users");
+
+// CONNECT TO MONGOOSE -> MONGO DB
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-// routes
-const users = require("./routes/api/users");
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
+// MUST HAPPEN FIRST, TO ENABLE JSON BODY PARSING
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// ROUTES
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
