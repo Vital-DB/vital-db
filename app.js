c60c62c6e4c969ef85ee428f7c7eeb480184bc08
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-13 16:56:19 -0400

    Change backend port to 5000

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ const users = require("./routes/api/users");
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
