8657a3574c16de5e6f4885f3169d5df95b6aa4c4
Alex Tong <tongsalex@gmail.com>
2020-06-14 16:01:45 -0400

    implementing a basic signup/login components

---
+++ frontend/src/components/session/Login.jsx
@@ -0,0 +1,26 @@
+import {connect} from 'react-redux';
+import {login} from '../../actions/session';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import e from 'express';
+
+export const Login = () => {
+    const dispatch = useDispatch();
+    const errors = useSelector(state => state.errors.session);
+    const [handle, setHandle] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let user = {
+            handle: handle,
+            password: password
+        }
+
+        dispatch(login(user));
+    }
+
+    const handleChange = (event) => {
+        
+    }
+}
