fcf0c1153813cbf916e105194cc13e8b712ff45d
“nahuel” <n.gorosito@yahoo.com>
2020-06-14 14:13:30 -0400

    installed axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
