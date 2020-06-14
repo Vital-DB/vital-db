0732acbb6305e593d34d668d71b2b298bce08eee
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-14 17:35:17 -0400

    Render NewRecord component and create route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Switch } from 'react-router-dom';
 import { GuestRoute, PrivateRoute } from '../util/route';
 
-import { Login} from './session/Login';
+import { Login } from './session/Login';
+import { NewRecord } from './records/NewRecord';
 
 const App = () => {
     return (
       <Switch>
         <GuestRoute exact path="/" component={Login} />
         <GuestRoute exact path="/login" component={Login} />
+        <GuestRoute exact path="/records/new" component={NewRecord} />
       </Switch>
     );
 }
