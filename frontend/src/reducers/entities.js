a634bedb04366a5c318ebc3a86e50d00cc60a024
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-15 16:13:41 -0400

    Create entities in entities reducer

--- frontend/src/reducers/entities.js
+++ frontend/src/reducers/entities.js
@@ -1,8 +1,24 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import cholesterol from './cholesterol'
+import bloodPressure from './bloodPressure'
+import weight from './weight.js'
+import vitaminD from './vitaminD'
+import restingHeartRate from './restingHeartRate'
+import temperature from './temperature'
+import allergy from './allergy'
+import medicalCondition from './medicalCondition'
 
 const entitiesReducer = combineReducers({
     user,
+    cholesterol,
+    bloodPressure,
+    weight,
+    vitaminD,
+    restingHeartRate,
+    temperature,
+    allergy,
+    medicalCondition,
 })
 
 export default entitiesReducer;
