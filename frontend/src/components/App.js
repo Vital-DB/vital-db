60f5b2c439aa2928ce55083d2d10692fb5ab2623
Alex Tong <tongsalex@gmail.com>
2020-06-14 20:36:38 -0400

    set up register and login components, added a reset css file, and a global css file for global styles

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Switch } from 'react-router-dom';
-import { GuestRoute, PrivateRoute } from '../util/route';
+import { GuestRoute } from '../util/route';
 
 import { Login} from './session/Login';
+import { Register } from './session/Register';
 
 const App = () => {
     return (
       <Switch>
-        <GuestRoute exact path="/" component={Login} />
         <GuestRoute exact path="/login" component={Login} />
+        <GuestRoute exact path="/register" component={Register} />
       </Switch>
     );
 }
