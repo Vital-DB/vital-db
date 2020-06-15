53e66f05433ab7bf8f7e4a519786bc55ee2d0b63
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-15 16:00:46 -0400

    Remove debugger

--- frontend/src/reducers/users.js
+++ frontend/src/reducers/users.js
@@ -6,7 +6,7 @@ export default (state = {}, action) => {
     Object.freeze(state)
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
-            // debugger;
+//             debugger;
             return {
                 [action.currentUser.data.currentUser._id]: action.currentUser.data.currentUser,
             }
