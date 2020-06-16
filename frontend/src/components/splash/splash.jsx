cab69f3c073e470160dde29d05c2db16822a8b40
“nahuel” <n.gorosito@yahoo.com>
2020-06-16 15:23:29 -0400

    got rid of dead code

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -6,9 +6,7 @@ import chart from './chart-icon.png';
 import remind from './med-remind6.png';
 
 class Splash extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  
 
   render() {
     return (
@@ -43,20 +41,16 @@ class Splash extends React.Component {
             </section>
             <section className="feature">
               <h1 id="heading">Connect with your doctor</h1>
-              {/* <img src="/images/docchat-icon.png" id="docchat"/> */}
               <img src={chat} id="docchat" alt="chat"/>
             </section>
             <section className="feature">
               <h1 id="heading">Easy access to charts</h1>
-              {/* <img src="/images/chart-icon.png" id="chart-icon"/> */}
               <img src={chart} id="chart-icon" alt="chart"/>
             </section>
             <section className="feature">
               <h1 id="heading">Medication reminders</h1>
-              {/* <img src="/images/med-remind6.png" id="med-remind"/> */}
               <img src={remind} id="med-remind" alt="remind"/>
             </section>
-            {/* <h1 id="heading">All your vitals stored in one secure space</h1> */}
           </div>
         </div>
           <div className="splash-navlinks">
