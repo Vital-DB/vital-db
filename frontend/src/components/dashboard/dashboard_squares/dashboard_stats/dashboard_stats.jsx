667915b08a2ba9da86a98a105649353aef8d1b5f
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-16 13:52:49 -0400

    Add display/hide to dashboard-list items

--- frontend/src/components/dashboard/dashboard_squares/dashboard_stats/dashboard_stats.jsx
+++ frontend/src/components/dashboard/dashboard_squares/dashboard_stats/dashboard_stats.jsx
@@ -9,6 +9,21 @@ class DashboardStats extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
 
+    showList(e){
+
+        debugger;
+        e.preventDefault();
+        const listItems = e.currentTarget.children;
+
+        for (let i = 0; i < listItems.length; i++){
+            e.currentTarget.children[i].style.display = "block";
+        }
+        
+        // for (let i = 0; i < listItems.length; i++){
+        //     e.currentTarget.parentNode.children[i].style.display = "none";
+        // }
+    }
+
     handleClick(e) {
         const dataKey = e.currentTarget.getAttribute('value');
         switch (dataKey) {
@@ -33,7 +48,7 @@ class DashboardStats extends React.Component {
             default:
                 break;
         } 
-        this.setState({dataKey: dataKey})
+        this.setState({dataKey: dataKey});
     }
 
     render(){
@@ -48,7 +63,7 @@ class DashboardStats extends React.Component {
         const dummyStats = [{weight: 220, restingHR: 75, date: '6/01/2020'}, {weight: 218, restingHR: 80, date: '6/12/2020'}, {weight: 210, restingHR: 78, date: '6/15/2020'}, {weight: 214, restingHR: 85, date: '6/16/2020'}]
         const data = vitals[this.state.dataKey];
 
-        debugger
+        
 
         const renderLineChart = (
             <ResponsiveContainer>
@@ -78,7 +93,7 @@ class DashboardStats extends React.Component {
                             <input type="radio" value='restingHR' name='dashboard-info' onClick={this.handleClick} />
                         </label>
                     </form> */}
-                    <ul className="dashboard-stats-list">
+                    <ul onClick={this.showList} className="dashboard-stats-list">
                         {/* values on the li are the vitals keys, displayed text uses regex to convert camelcase to capitalized first letter with spaces in between */}
                         {/* exclude values if they don't have numeric stats */}
                         {Object.keys(vitals).map((vitalName, idx) => (!dontInclude.includes(vitalName)) ? <li key={idx} value={vitalName} onClick={this.handleClick}>{vitalName.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })}</li> : "")}
