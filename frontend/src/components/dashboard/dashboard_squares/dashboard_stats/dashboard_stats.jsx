eff042845b6f7ef164b3d54e56abdb2b20835bb9
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-16 21:38:47 -0400

    Implement dynamic graph feature

--- frontend/src/components/dashboard/dashboard_squares/dashboard_stats/dashboard_stats.jsx
+++ frontend/src/components/dashboard/dashboard_squares/dashboard_stats/dashboard_stats.jsx
@@ -6,8 +6,12 @@ import {Route} from 'react-router-dom'
 class DashboardStats extends React.Component {
     constructor(props){
         super(props);
-        this.state = {dataKey: 'cholesterolLevels'}
+        this.state = {
+            dataKey: 'cholesterolLevels', 
+            subDataKeys: ["All"],
+        }
         this.handleClick = this.handleClick.bind(this);
+        this.handleClickSub = this.handleClickSub.bind(this);
     }
 
     componentDidMount(){
@@ -17,6 +21,20 @@ class DashboardStats extends React.Component {
         this.props.fetchRestingHeartRates();  
         this.props.fetchBloodPressureLevels();   
         this.props.fetchCholesterolLevels();
+    }
+
+    componentDidUpdate(prevProps, prevState){
+
+        // reset subDataKeys and selected-sub when user clicks on another dataKey
+       if (this.state.dataKey !== prevState.dataKey){
+           this.setState({subDataKeys: ["All"]});
+           let selected = document.querySelectorAll('.dashboard-stats .selected-sub');
+           for (let i = 0; i < selected.length; i++){
+               debugger
+               selected[i].classList.remove('selected-sub');
+           }
+           document.querySelectorAll('.dashboard-stats-sublist li')[0].classList.add('selected-sub')
+       }
     }
 
     handleClick(e) {
@@ -47,6 +65,42 @@ class DashboardStats extends React.Component {
 
     handleClickSub(e){
         
+        let subDataKeys = this.state.subDataKeys;
+        let target = e.currentTarget;
+        let dataKey = target.getAttribute('value');
+        let selectedSubs = [];
+
+        if (dataKey === 'All'){
+            target.classList.add('selected-sub');
+            let allSubs = target.parentNode.children;
+            for(let i = 0; i < allSubs.length; i++){
+                debugger
+                let currentSub = allSubs[i];
+                selectedSubs.push(currentSub.getAttribute('value'));
+                if (i !== 0) currentSub.classList.remove('selected-sub');
+            }
+        } else {
+            let allDataKey = target.parentNode.children[0];
+            if (subDataKeys[0] === "All" || (allDataKey.getAttribute('value') === 'All') && allDataKey.classList.contains('selected-sub') ) {
+                subDataKeys = [];
+                allDataKey.classList.remove('selected-sub');
+            }
+
+            if (subDataKeys.includes(dataKey) && !subDataKeys.includes('All')){
+                target.classList.remove('selected-sub');
+                const removedIdx = subDataKeys.indexOf(dataKey);
+                const first = subDataKeys.slice(0, removedIdx);
+                const last = subDataKeys.slice(removedIdx+1);
+
+                selectedSubs = first.concat(last);
+            } else {
+                selectedSubs.pop();
+                target.classList.add('selected-sub');
+                subDataKeys.push(dataKey);
+                selectedSubs = subDataKeys;
+            }
+        }        
+        this.setState({subDataKeys: selectedSubs})
     }
 
     render(){
@@ -61,10 +115,29 @@ class DashboardStats extends React.Component {
         // const dummyStats = [{weight: 220, restingHR: 75, date: '6/01/2020'}, {weight: 218, restingHR: 80, date: '6/12/2020'}, {weight: 210, restingHR: 78, date: '6/15/2020'}, {weight: 214, restingHR: 85, date: '6/16/2020'}]
         const data = vitals[this.state.dataKey];
 
+        let subVitals = (function(vital) {
+            switch(vital) {
+              case 'bloodPressureLevels':
+                return ["systolic", "diastolic"]
+              case 'cholesterolLevels':
+                return ["LDL", "HDL", "total", "triglycerides"]
+              default:
+                return ["value"];
+            }
+          })(this.state.dataKey);
+
+        let chartLines; 
+        if (this.state.subDataKeys.includes('All') & this.state.subDataKeys.length == 1){
+            chartLines = subVitals;
+        } else {
+            chartLines = this.state.subDataKeys;
+        }
+    
+
         let renderLineChart = (
             <ResponsiveContainer>
                 <LineChart data={data}>
-                    <Line type="monotone" dataKey={"value"} fill="#7cc5f5" />
+                    {chartLines.map((subVital, idx) => <Line key={idx} type="monotone" dataKey={subVital} fill="#7cc5f5" /> )}
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey='date' />
                     <YAxis domain={['dataMin - 5', 'dataMax + 5']} />
@@ -73,27 +146,27 @@ class DashboardStats extends React.Component {
             </ResponsiveContainer>
         );
 
-        switch (this.state.dataKey) {
-            case "bloodPressureLevels":
-                renderLineChart = (
-                    <ResponsiveContainer>
-                    <LineChart data={data}>
-                        <Line type="monotone" dataKey={"systolic"} fill="#7cc5f5" />
-                        <Line type="monotone" dataKey={"diastolic"} fill="#7cc5f5" />
-                        <CartesianGrid stroke="#ccc" />
-                        <Tooltip/>
-                        <XAxis dataKey='date' />
-                        <YAxis domain={['dataMin - 5', 'dataMax + 5']} />
-                    </LineChart>
-                    </ResponsiveContainer>
-                )
-                break;
+        // switch (this.state.dataKey) {
+        //     case "bloodPressureLevels":
+        //         renderLineChart = (
+        //             <ResponsiveContainer>
+        //             <LineChart data={data}>
+        //                 <Line type="monotone" dataKey={"systolic"} fill="#7cc5f5" />
+        //                 <Line type="monotone" dataKey={"diastolic"} fill="#7cc5f5" />
+        //                 <CartesianGrid stroke="#ccc" />
+        //                 <Tooltip/>
+        //                 <XAxis dataKey='date' />
+        //                 <YAxis domain={['dataMin - 5', 'dataMax + 5']} />
+        //             </LineChart>
+        //             </ResponsiveContainer>
+        //         )
+        //         break;
         
-            default:
-                break;
-        }
+        //     default:
+        //         break;
+        // }
 
-
+        //   debugger
         return(
 
             <div id='my-dashboard-stats' className='dashboard-stats'>
@@ -116,7 +189,8 @@ class DashboardStats extends React.Component {
                         {Object.keys(vitals).map((vitalName, idx) => (!dontInclude.includes(vitalName)) ? <li key={idx} value={vitalName} className={(idx == 0) ? "selected" : ""} onClick={this.handleClick}>{vitalName.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })}</li> : "")}
                     </ul>
                     <ul className="dashboard-stats-sublist">
-                        {Object.keys(data).map((subVital, idx) => <li key={idx} value={subVital} onClick={this.handleClickSub}>{subVital}</li>)}
+                        <li value="All" className="selected-sub" onClick={this.handleClickSub}>All</li>
+                        {subVitals.map((subVital, idx) => <li key={idx} value={subVital} onClick={this.handleClickSub}>{subVital}</li>)}
                     </ul>
                 </div>
                 {renderLineChart}
