0ba6397e70dd41f97236cc46952cf3edb47400a2
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-17 19:30:49 -0400

    Add 4 colors for graph

--- frontend/src/components/dashboard/dashboard_squares/dashboard_stats/DashboardStatsGraph.jsx
+++ frontend/src/components/dashboard/dashboard_squares/dashboard_stats/DashboardStatsGraph.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import { LineChart, Line, CartesianGrid, YAxis, XAxis, ResponsiveContainer, Tooltip } from 'recharts';
 
-export default ({chartLines, data}) => (
-    <ResponsiveContainer>
-        <LineChart data={data}>
-            {chartLines.map((subVital, idx) => <Line key={idx} type="monotone" dataKey={subVital} fill="#7cc5f5" /> )}
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey='date' />
-            <YAxis domain={['dataMin - 5', 'dataMax + 5']} />
-            <Tooltip/>
-        </LineChart>
-    </ResponsiveContainer>
-);
+export default ({chartLines, data}) => {
+    const colors = ["#FF4E66","#FF5733","#86FF33","#6337D8"]
+
+    // use with attribute 'content={<CustomTooltip />' on line, for custom tooltip}
+    // const CustomTooltip = ({ payload, label, active }) => {
+    //     // debugger
+    //     if (active) {
+    //         return (
+    //             <div className="custom-tooltip">
+    //                 <p className="label">{`${label}`}</p>
+    //             </div>
+    //         );
+    //     }
+    
+    //     return null;
+    // }
+
+    return (
+        <ResponsiveContainer>
+            <LineChart data={data}>
+                {chartLines.map((subVital, idx) => <Line key={idx} filterNull={true} connectNulls={true} activeDot={{ stroke: 'red', strokeWidth: 2, r: 10 }} dot={false} pointstype="monotone" strokeWidth="5" dataKey={subVital} stroke={colors[idx]} /> )}
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey='date' />
+                <YAxis domain={['dataMin - 5', 'dataMax + 5']} />
+                <Tooltip/>
+            </LineChart>
+        </ResponsiveContainer>
+    )
+}
 
 
