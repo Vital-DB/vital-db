7e864c33c9c110e840ea448e957e69dd34f7367f
Clifford Yan <clifford.syan@gmail.com>
2020-06-17 16:48:17 -0400

    Replaced hamburger button with danc pill

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -5,7 +5,7 @@ import SideNavContainer from '../side_nav/side_nav_container';
 import NewPicture from '../pictures/NewPicture';
 import editFormContainer from '../edit_form/editFormContainer';
 import Allergies from '../allergies/Allergies';
-import danc from './danc.png';
+// import danc from './danc.png';
 import './main.css';
 
 class Main extends React.Component {
@@ -15,32 +15,11 @@ class Main extends React.Component {
 
     render(){
 
-        const {handle} = this.props;
-        const date = new Date();
-        const hours = date.getHours();
-        let greeting = '';
-
-        if(hours >= 4 && hours <= 5) {
-            greeting = `Wow ${handle}, you're up early!`
-        } else if(hours < 12 && hours >= 6) {
-            greeting = `Good morning ${handle}!`;
-        } else if (hours >= 12 && hours <= 17) {
-            greeting = `Good afternoon ${handle}`;
-        } else if (hours > 17 && hours <= 21) {
-            greeting = `Good evening ${handle}!`;
-        } else if (hours >= 22 && hours <= 24) {
-            greeting = `Good night ${handle}!`;
-        } else if (hours >= 1 && hours <= 3) {
-            greeting = `Sleep is important for the body ${handle}!`;
-        }
 
         return(
             <div className='vital-main'>
                 <PrivateRoute path='/main' component={SideNavContainer} />
-                <div className='danc-talk'>
-                    <div className='speech-bubble'>{greeting}</div>
-                    <img src={danc} width='100' height='140' className='danc' alt=""/>
-                </div>
+                
                 <PrivateRoute path='/main/dashboard' component={DashboardContainer} />
                 <PrivateRoute exact path="/main/pictures/new" component={NewPicture} />
                 <PrivateRoute path='/main/edit' component={editFormContainer} />
