36fcc751b4dfe8e8a9874134396705d4d06ac325
Alex Tong <tongsalex@gmail.com>
2020-07-15 16:54:15 -0400

    added in page for bios

---
+++ frontend/src/components/about/About.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './about.css';
+
+export default () => {
+    return (
+        <div className='about'>
+            <section className='about__bio'>
+                <img src="https://avatars0.githubusercontent.com/u/21978330?s=460&u=a8dea8d9e1f18aa9f3e0ea489090f4fc1531453b&v=4" className='about__profilePic' />
+                    <h1>Alex Tong</h1>
+                    <h1>Backend Lead</h1>
+                    <div className="about__linksContainer">
+                        <a href="http://tongsalex.com" 
+                        target="_blank" 
+                        className="about__links">Portfolio</a>
+                        <a href="http://linkedin.com/in/tongsalex" 
+                        target="_blank"
+                        className="about__links">LinkedIn</a>
+                        <a href="http://github.com/tongsalex" 
+                        target="_blank"
+                        className="about__links">GitHub</a>
+                    </div>
+            </section>
+
+            <section className='about__bio'>
+                <img src="" className='about__profilePic' />
+                    <h1>name</h1>
+                    <h1>role</h1>
+                    <div className="about__linksContainer">
+                        <a href="http://.com" 
+                        target="_blank" 
+                        className="about__links">Portfolio</a>
+                        <a href="http://linkedin.com/in/" 
+                        target="_blank"
+                        className="about__links">LinkedIn</a>
+                        <a href="http://github.com/" 
+                        target="_blank"
+                        className="about__links">GitHub</a>
+                    </div>
+            </section>
+
+            <section className='about__bio'>
+                <img src="" className='about__profilePic' />
+                    <h1>name</h1>
+                    <h1>role</h1>
+                    <div className="about__linksContainer">
+                        <a href="http://.com" 
+                        target="_blank" 
+                        className="about__links">Portfolio</a>
+                        <a href="http://linkedin.com/in/" 
+                        target="_blank"
+                        className="about__links">LinkedIn</a>
+                        <a href="http://github.com/" 
+                        target="_blank"
+                        className="about__links">GitHub</a>
+                    </div>
+            </section>
+
+            <section className='about__bio'>
+                <img src="" className='about__profilePic' />
+                    <h1>name</h1>
+                    <h1>role</h1>
+                    <div className="about__linksContainer">
+                        <a href="http://.com" 
+                        target="_blank" 
+                        className="about__links">Portfolio</a>
+                        <a href="http://linkedin.com/in/" 
+                        target="_blank"
+                        className="about__links">LinkedIn</a>
+                        <a href="http://github.com/" 
+                        target="_blank"
+                        className="about__links">GitHub</a>
+                    </div>
+            </section>
+        </div>
+    )
+}
